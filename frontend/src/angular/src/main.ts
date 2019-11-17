2be6a91845117cf0beae99f9f67cbbf3e4f521bb
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2019-11-17 01:55:23 +0100

    Update to last version of Angular and remove angular warnings

--- frontend/src/angular/src/main.ts
+++ frontend/src/angular/src/main.ts
@@ -1,8 +1,7 @@
 import './polyfills';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
 
 if (environment.production) {
